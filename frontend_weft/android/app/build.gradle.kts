79b97dfcead15c695020332479d0706429fbec21
TANICE-GAWD <2006princesharma@gmail.com>
2025-07-12 16:16:55 +0530

    additions

--- frontend_weft/android/app/build.gradle.kts
+++ frontend_weft/android/app/build.gradle.kts
@@ -3,6 +3,13 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.3" apply false
+    id("com.google.gms.google-services")
+}
+
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
 }
 
 android {
@@ -21,7 +28,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.frontend_weft"
+        applicationId = "com.weftconnectspeople.weft"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
